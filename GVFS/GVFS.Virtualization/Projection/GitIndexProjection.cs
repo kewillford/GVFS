73debcfe6985cedb5e85c6c115aa314871fabbe2
Kevin Willford <kewillf@microsoft.com>
2019-05-16 10:26:54 -0600

    Remove Contains from the placeholder interface and use in memory hashset
    Using this because Contains was only being used on Mac and is only used
    when updating placeholders.  Using the in memory hashset is much faster and
    will not affect the Windows code.

--- GVFS/GVFS.Virtualization/Projection/GitIndexProjection.cs
+++ GVFS/GVFS.Virtualization/Projection/GitIndexProjection.cs
@@ -1116,11 +1116,14 @@ namespace GVFS.Virtualization.Projection
                 int deleteFolderPlaceholderAttempted = 0;
                 using (BlobSizes.BlobSizesConnection blobSizesConnection = this.blobSizes.CreateConnection())
                 {
-                    // A hash of the folder placeholders is only required if the platform expands directories
-                    HashSet<string> folderPlaceholders =
-                        GVFSPlatform.Instance.KernelDriver.EnumerationExpandsDirectories ?
-                        new HashSet<string>(placeholderFoldersListCopy.Select(x => x.Path), StringComparer.OrdinalIgnoreCase) :
-                        null;
+                    // A hash of the placeholders is only required if the platform expands directories
+                    HashSet<string> existingPlaceholders = null;
+                    if (GVFSPlatform.Instance.KernelDriver.EnumerationExpandsDirectories)
+                    {
+                        IEnumerable<string> allPlaceholders = placeholderFoldersListCopy.Select(x => x.Path)
+                            .Union(placeholderFilesListCopy.Select(x => x.Path));
+                        existingPlaceholders = new HashSet<string>(allPlaceholders, StringComparer.OrdinalIgnoreCase);
+                    }
 
                     // Order the folders in decscending order so that we walk the tree from bottom up.
                     // Traversing the folders in this order:
@@ -1159,7 +1162,7 @@ namespace GVFS.Virtualization.Projection
                             // properly
                             if (GVFSPlatform.Instance.KernelDriver.EnumerationExpandsDirectories && folderPlaceholder.IsExpandedFolder)
                             {
-                                this.ReExpandFolder(blobSizesConnection, folderPlaceholder.Path, folderPlaceholders);
+                                this.ReExpandFolder(blobSizesConnection, folderPlaceholder.Path, existingPlaceholders);
                             }
                         }
                         else
@@ -1338,7 +1341,7 @@ namespace GVFS.Virtualization.Projection
         private void ReExpandFolder(
             BlobSizes.BlobSizesConnection blobSizesConnection,
             string relativeFolderPath,
-            HashSet<string> existingFolderPlaceholders)
+            HashSet<string> existingPlaceholders)
         {
             FolderData folderData;
             if (!this.TryGetOrAddFolderDataFromCache(relativeFolderPath, out folderData))
@@ -1369,9 +1372,7 @@ namespace GVFS.Virtualization.Projection
                     childRelativePath = relativeFolderPath + Path.DirectorySeparatorChar + childEntry.Name.GetString();
                 }
 
-                bool newChild = childEntry.IsFolder ? !existingFolderPlaceholders.Contains(childRelativePath) : !this.placeholderDatabase.Contains(childRelativePath);
-
-                if (newChild)
+                if (!existingPlaceholders.Contains(childRelativePath))
                 {
                     FileSystemResult result;
                     if (childEntry.IsFolder)
@@ -1402,7 +1403,7 @@ namespace GVFS.Virtualization.Projection
                             // Git command must have removed the folder being re-expanded (relativeFolderPath)
                             // Remove the folder from existingFolderPlaceholders so that its parent will create
                             // it again (when it's re-expanded)
-                            existingFolderPlaceholders.Remove(relativeFolderPath);
+                            existingPlaceholders.Remove(relativeFolderPath);
                             this.placeholderDatabase.Remove(relativeFolderPath);
                             return;
 
